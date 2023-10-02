761092f1d7b34b4eea567fab971b1a8eed5367a3
Yerkin <yerkinmm@gmail.com>
2023-10-02 17:59:02 +0600

    popup msg

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -11,18 +11,14 @@ export const App: React.FC<AppProps> = ({ className }) => {
 
   const showPopupOnClick = async () => {
     const message =
-      "Thanks for using react-mini-app! I hope it helps you to create awesome Telegram Mini apps!";
+      "Hi this is my first mini app and it's just popup message!";
     await showPopup({ title: "Hey!", message: message });
   };
 
   return (
     <div className={clsx(styles.App, className)}>
       <div className={styles.container}>
-        <a
-          href="https://github.com/usernein/react-mini-app"
-          target="_blank"
-          className={styles.githubLink}
-        >
+        <a target="_blank" className={styles.githubLink}>
           <h1 className={styles.title}>react-mini-app</h1>
         </a>
 
